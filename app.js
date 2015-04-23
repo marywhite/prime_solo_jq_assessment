49a4f086b730b2ea4f5ea093c518b5fc06bc469e
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-23 15:56:00 -0500

    working toggle

--- app.js
+++ app.js
@@ -9,12 +9,23 @@
 $(document).ready(function (){
 
 	var i = 0;
+	//append new div with text and buttons
 	$('button').click(function(){
 		i++;
 		$('#container').append("<div>Line # " + i + "<button class='changeColor'>Change Color</button> <button class='removeButton'>Remove</button></div>");
 	});
 
+	//change background color of clicked div
+	$("#container").on('click', '.changeColor',function() {
+		$(this).parent().toggleClass('selected');
+	});
+
+	//remove clicked div
 	$("#container").on('click', '.removeButton',function() {
 		$(this).parent().remove();
 	});
+	
 });
+
+
+
