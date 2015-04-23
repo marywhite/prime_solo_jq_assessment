05a310e08ded9e2f169bdde1828b1e8d090158b7
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-23 15:38:15 -0500

    working generate and remove

---
+++ app.js
@@ -0,0 +1,20 @@
+// // In your HTML, include a ‘generate’ button, 
+// // that when clicked, will generate a new div container 
+// // in the app.js. The new div should include some text 
+// // that reads “Line #” and then a number specific to the number of time the generate button has been clicked. 
+// In addition to the text, you will need to include 
+// two buttons within the new div. One button will be entitled 
+// // ‘Change Color’ and the other button will entitled “Remove”.
+
+$(document).ready(function (){
+
+	var i = 0;
+	$('button').click(function(){
+		i++;
+		$('#container').append("<div>Line # " + i + "<button class='changeColor'>Change Color</button> <button class='removeButton'>Remove</button></div>");
+	});
+
+	$("#container").on('click', '.removeButton',function() {
+		$(this).parent().remove();
+	});
+});
