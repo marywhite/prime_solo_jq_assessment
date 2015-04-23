de0254362efc44477dfa76f32e3e5228d3c36b14
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-23 16:38:35 -0500

    tennis ballin

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// // In your HTML, include a ‘generate’ button, 
-// // that when clicked, will generate a new div container 
-// // in the app.js. The new div should include some text 
-// // that reads “Line #” and then a number specific to the number of time the generate button has been clicked. 
-// In addition to the text, you will need to include 
-// two buttons within the new div. One button will be entitled 
-// // ‘Change Color’ and the other button will entitled “Remove”.
 
 $(document).ready(function (){
 
@@ -12,7 +5,7 @@ $(document).ready(function (){
 	//append new div with text and buttons
 	$('button').click(function(){
 		i++;
-		$('#container').append("<div>Line # " + i + "<button class='changeColor'>Change Color</button> <button class='removeButton'>Remove</button></div>");
+		$('#container').append("<div class='newEntry'>Line # " + i + "<button class='changeColor'>Change Color</button> <button class='removeButton'>Remove</button></div>");
 	});
 
 	//change background color of clicked div
@@ -22,7 +15,7 @@ $(document).ready(function (){
 
 	//remove clicked div
 	$("#container").on('click', '.removeButton',function() {
-		$(this).parent().remove();
+		$(this).parent().fadeOut("600");
 	});
 	
 });
